f6ad7d87489cb5df5980c97550b6e5c96e1bcf23
Bob Rudis <bob@rudis.net>
2015-03-23 17:35:32 -0400

    read_palette

---
+++ R/readpal.R
@@ -0,0 +1,28 @@
+#' Read colors from palette files
+#'
+#' Given a path or URL to an palette file, this function will attempt to determine
+#' which palette file format to read by the file type and return
+#' a named character vector (if color names are present) of hex RGB colors.
+#'
+#' @param path partial or full file path or URL to a GPL file
+#' @param use_names add color names to the vector (defaults to \code{TRUE}). See NOTE
+#' @param .verbose show extra information about GPL file processing
+#' @note When using named color palettes in a \code{ggplot2} \code{scale_} context, you
+#'     must \code{unname} them or set \code{use_names} to \code{FALSE}.
+#'     Not sure if this is a bug or a deliberate feature in ggplot2.
+#' @export
+read_palette <- function(path, use_names=TRUE, .verbose=FALSE) {
+
+  ext <- file_ext(path)
+
+  if (ext == "aco") { return(read_aco(path, use_names, .verbose)) }
+  if (ext == "ase") { return(read_ase(path, use_names, .verbose)) }
+  if (ext == "gpl") { return(read_gpl(path, use_names, .verbose)) }
+  if (ext == "kde") { return(read_kde(path, use_names, .verbose)) }
+  if (ext == "soc") { return(read_soc(path, use_names, .verbose)) }
+
+  message("Unrecognized or unsupported palette file format")
+
+  return(NULL)
+
+}
--- R/swatches-package.R
+++ R/swatches-package.R
@@ -2,6 +2,7 @@
 #' @name swatches
 #' @docType package
 #' @import pack stringr
+#' @importFrom tools file_ext
 #' @importFrom XML xmlParse
 #' @importFrom XML xpathSApply
 #' @importFrom XML xmlAttrs
